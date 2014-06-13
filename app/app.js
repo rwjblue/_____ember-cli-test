75c48ac85fdec29ede99b6c7523eb447a0bda780
Robert Jackson <robert.w.jackson@me.com>
2014-06-13 07:46:16 -0400

    Resolve pod-based components properly.

--- app/app.js
+++ app/app.js
@@ -2,11 +2,24 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 
+var CustomResolver = Resolver.extend({
+  podBasedModuleName: function(parsedName){ 
+    var podPrefix = this.namespace.podModulePrefix || this.namespace.modulePrefix;
+    var fullNameWithoutType = parsedName.fullNameWithoutType;
+
+    if (parsedName.type === 'template') {
+      fullNameWithoutType = fullNameWithoutType.replace(/^components\//, '');
+    }
+
+    return podPrefix + '/' + fullNameWithoutType + '/' + parsedName.type;
+  }
+});
+
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
   modulePrefix: 'some-test', // TODO: loaded via config
-  Resolver: Resolver
+  Resolver: CustomResolver
 });
 
 loadInitializers(App, 'some-test');
