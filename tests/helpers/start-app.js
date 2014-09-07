fa883f8cc14bba0cd027f9107ce16446a7a4a822
Robert Jackson <robert.w.jackson@me.com>
2014-09-07 10:54:07 -0400

    Update to latest ember-cli (45e80884948b9e0b5e0b5bf18f89e5c4a41ae7d5).

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,16 +1,12 @@
 import Ember from 'ember';
 import Application from 'some-test/app';
 import Router from 'some-test/router';
+import config from 'some-test/config/environment';
 
 export default function startApp(attrs) {
   var App;
 
-  var attributes = Ember.merge({
-    // useful Test defaults
-    rootElement: '#ember-testing',
-    LOG_ACTIVE_GENERATION: false,
-    LOG_VIEW_LOOKUPS: false
-  }, attrs); // but you can override;
+  var attributes = Ember.merge(config, attrs); // use defaults, but you can override;
 
   Router.reopen({
     location: 'none'
